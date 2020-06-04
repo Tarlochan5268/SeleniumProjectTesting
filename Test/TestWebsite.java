4c986ad495579b0f73be66c89797175de7bf5f78
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-04 07:16:11 -0400

    test Two Input Field in Form worked tested website done

--- Test/TestWebsite.java
+++ Test/TestWebsite.java
@@ -35,27 +35,27 @@ public class TestWebsite
 
     @Test
     public void testSingleInputField() throws InterruptedException  {
-        /*
-         * 1. FIND the textbox    (id = user-message)
-         * 2. Type "HELLO WORLD" into the textbox (.sendKeys("..."))
-         * 3. FIND the button  (cssSelectors = form#get-input button)
-         * 4. CLICK on the button (.click())
-         * 5. FIND the output message thing (id = display)
-         * 6. CHECK the output message  (expectedResult = "HELLO WORLD")
-         * 			(.getText());
-         */
-
         WebElement textBox = driver.findElement(By.id("user-message"));
         textBox.sendKeys("Das");
-
         WebElement button = driver.findElement(By.cssSelector("form#get-input button"));
         button.click();
-
         WebElement outputSpan = driver.findElement(By.id("display"));
         String outputMessage = outputSpan.getText();		//actual output
-
         assertEquals("Das", outputMessage);
+    }
 
+    @Test
+    public void testTwoFieldsInputs() throws InterruptedException
+    {
+        WebElement textBoxa = driver.findElement(By.id("sum1"));
+        textBoxa.sendKeys("10"); //VALUE A FIELD
+        WebElement textBoxb = driver.findElement(By.id("sum2"));
+        textBoxb.sendKeys("20"); //VALUE B FIELD
 
+        WebElement button = driver.findElement(By.cssSelector("form#gettotal button"));
+        button.click();
+        WebElement outputSpan = driver.findElement(By.id("displayvalue"));
+        String outputMessage = outputSpan.getText();		//actual output
+        assertEquals("30", outputMessage);
     }
 }
